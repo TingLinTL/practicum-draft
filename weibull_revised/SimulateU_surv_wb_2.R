d3ed21e07983c9425325a4c4ac0a05d5ddec036b
TingLinTL <Tarynlin0101@gmail.com>
2025-05-20 13:35:18 -0400

    draft

---
+++ weibull_revised/SimulateU_surv_wb_2.R
@@ -0,0 +1,107 @@
+SimulateU_surv <- function(t, d, z, x, zetat, zetaz, theta = 0.5, iter = 20, weights = NULL, offset = FALSE){
+  #t is a vector of n, time to event
+  #d is a vector of n, indicator of event
+  #z is a vector of n, treatment
+  #x is a matrix, covariates
+  #zetat is a scaler, zetaz is a scaler, sensitivity parameters
+  
+  n = length(t) #number of observations
+  x = data.matrix(x)
+  nx = dim(x)[2] #number of observed covariates
+  
+  p = theta
+  U = rbinom(n,1,p)
+  #Upath = U (useful for checking convergence)
+  
+  for(j in 1:iter){
+    if(offset){
+      # Fit Weibull AFT with offset for U
+      U.fit1 = survreg(Surv(t, d) ~ z + x + offset(U * zetat), weights = weights, dist = "weibull")
+      t.coef1 = c(U.fit1$coef, zetat)  #zetat as fixed effect
+      
+      # Fit treatment model with offset for U
+      z.fit = glm(z ~ x, family = binomial(link = "probit"), 
+                  offset = zetaz * U,
+                  control = glm.control(epsilon = 1e-6, maxit = 50))
+      z.coef = c(z.fit$coef, zetaz)  # append zetaz as fixed effect
+    } else {
+      #fit time to event
+      U.fit1 = survreg(Surv(t,d) ~ z + x + U, weights=weights, dist = "weibull")
+      t.coef1 = U.fit1$coef
+      t.coef1[length(t.coef1)]  = zetat
+      #fit treatment
+      z.coef = glm(z ~ x + U, family = binomial(link="probit"), control = glm.control(epsilon = 1e-6, maxit = 50))$coef
+      z.coef[length(z.coef)] = zetaz
+      
+    }
+    
+    t.coef1[is.na(t.coef1)] = 0
+    z.coef[is.na(z.coef)] = 0
+    
+    
+    if(offset){
+      sigma <- U.fit1$scale                        # AFT scale
+      v <- 1 / sigma                               # Weibull shape
+      Xmat <- model.matrix(~ z + x)
+      
+      # U = 1
+      Xmat1 <- cbind(Xmat, U = 1)
+      lp1 <- Xmat1 %*% matrix(t.coef1, ncol = 1)
+      lambda1 <- exp(-lp1 / sigma)
+      
+      # U = 0
+      Xmat0 <- cbind(Xmat, U = 0)
+      lp0 <- Xmat0 %*% matrix(t.coef1, ncol = 1)
+      lambda0 <- exp(-lp0 / sigma)
+      
+      # Treatment model matrix
+      Zmat1 <- cbind(1, x, 1)
+      Zmat0 <- cbind(1, x, 0)
+      
+      # Compute posterior probabilities
+      ptzu1 <- (1 - pnorm(Zmat1 %*% matrix(z.coef, ncol = 1)))^(1 - z) *
+        pnorm(Zmat1 %*% matrix(z.coef, ncol = 1))^z * theta *
+        (v * lambda1^v * t^(v - 1))^d *
+        exp(-lambda1^v * t^v)
+      
+      ptzu0 <- (1 - pnorm(Zmat0 %*% matrix(z.coef, ncol = 1)))^(1 - z) *
+        pnorm(Zmat0 %*% matrix(z.coef, ncol = 1))^z * (1 - theta) *
+        (v * lambda0^v * t^(v - 1))^d *
+        exp(-lambda0^v * t^v)
+    }
+    else{
+      sigma <- U.fit1$scale                        # AFT scale
+      v <- 1 / sigma                               # Weibull shape
+      Xmat <- model.matrix(~ z + x + U) 
+      
+      #For U=1 
+      Xmat1 <- Xmat
+      Xmat1[, "U"] <- 1
+      lp1 <- Xmat1 %*% matrix(t.coef1, ncol = 1)
+      lambda1 <- exp(-lp1/sigma)
+      #For U=0 
+      Xmat0 <- Xmat
+      Xmat0[, "U"] <- 0
+      lp0 <- Xmat0 %*% matrix(t.coef1, ncol = 1)
+      lambda0 <- exp(-lp0/sigma)
+      
+      ptzu1 = (1-pnorm(cbind(1,x,1)%*%matrix(z.coef, ncol = 1)))^(1-z)*
+        pnorm(cbind(1,x,1)%*%matrix(z.coef, ncol = 1))^z*theta*
+        (v * lambda1^v * t^(v - 1))^d * 
+        exp(-exp((log(t)-lp1)/sigma))
+      
+      ptzu0 = (1-pnorm(cbind(1,x,0)%*%matrix(z.coef, ncol = 1)))^(1-z)*
+        pnorm(cbind(1,x,0)%*%matrix(z.coef, ncol = 1))^z*theta*
+        (v * lambda0^v * t^(v - 1))^d * 
+        exp(-exp((log(t)-lp0)/sigma))
+    }
+    
+    p = ptzu1/(ptzu1 + ptzu0)
+    p[ptzu1==0 & ptzu0==0] = 0
+    
+    U = rbinom(n,1,p)
+    #Upath = cbind(Upath, U)
+  }
+  
+  return(list(U = U, p = p))
+}
---
+++ weibull_revised/surv_stoEM_ipw_wb_2.R
@@ -0,0 +1,65 @@
+surv_stoEM_ipw <- function(data, zetat, zetaz, B = 100, theta = 0.5){
+  t = data$M
+  d = data$status
+  Z = data$A
+  X = data.matrix(data[,c("x1","x2")])
+  nx = 2
+  n = length(t)
+  
+  
+  
+  #Record coefficients with simulated U
+  Coeft1 = Coeft1.se =spce = numeric(B)
+  partialR2z = c()
+  partialR2t1 = c()
+  
+  for (j in 1:B){
+    Usim = SimulateU_surv(t, d, Z, X, zetat = zetat, zetaz = zetaz, theta = theta, offset=FALSE)
+    
+    Z.fit = glm(Z ~ X, offset = zetaz * Usim$U, family=binomial(link="probit"))
+    # Calculate partial R-sq of z ~ u | x
+    Z.fit_reduced = glm(Z ~ X, family=binomial(link="probit"))
+    # if (zetaz >= 0)
+    #   partialR2z[j] = 1 - exp((Z.fit$deviance-Z.fit_reduced$deviance)/n)
+    # else
+    #   partialR2z[j] = - (1 - exp((Z.fit$deviance-Z.fit_reduced$deviance)/n))
+    
+    partialR2z[j] <- ifelse(zetaz >= 0,  1 - exp((Z.fit$deviance-Z.fit_reduced$deviance)/n), - (1 - exp((Z.fit$deviance-Z.fit_reduced$deviance)/n)))
+    
+    #ps = pnorm(cbind(1,X,Usim$U) %*% c(Z.fit$coefficients, zetaz))
+    ps = Z.fit$fitted.values
+    ipw = (sum(Z)/n) * Z/ps + (1-(sum(Z)/n))*(1-Z)/(1-ps)
+    ipw = pmin(ipw, 10)
+    ipw = pmax(ipw, 0.1)
+    
+    t1.ipw = survreg(Surv(t, d) ~ Z, weights = ipw, robust = TRUE, dist = "weibull")
+    mu1 <- predict(t1.ipw, newdata=data.frame(Z=c(rep(1,n))), type="lp")
+    mu0 <- predict(t1.ipw, newdata=data.frame(Z=c(rep(0,n))), type="lp")
+    sigma <-  t1.ipw$scale
+    S1 <- exp(-exp((log(t_pred)-mu1)/sigma))
+    S0 <- exp(-exp((log(t_pred)-mu0)/sigma))
+    spce[j] <- mean(S1)-mean(S0)
+    
+    
+    
+    
+    # Calculate partial R-sq of (t, d) ~ u | x, z
+    t1.fit = coxph(Surv(t, d) ~ X + Z + offset(zetat * Usim$U))
+    t1.fit_reduced = coxph(Surv(t, d) ~ X + Z)
+    logtest <- -2 * (t1.fit_reduced$loglik[2] - t1.fit$loglik[2])
+    if (zetat >= 0)
+      partialR2t1[j] = (1 - exp(-logtest/t1.fit$nevent))
+    else
+      partialR2t1[j] = - (1 - exp(-logtest/t1.fit$nevent))
+  }
+  
+  # names(Coeft1) =  names(t1.ipw$coefficients)
+  
+  tau1 = mean(Coeft1)
+  spce = mean(spce)
+  # tau1.se = sqrt(mean((Coeft1.se)^2) + (1+1/B) * var(Coeft1))
+  pR2z = mean(partialR2z)
+  pR2t1 = mean(partialR2t1)
+  
+  return (list(tau1 = tau1, spce = spce, pR2z = pR2z, pR2t1 = pR2t1))
+}
