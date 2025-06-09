5111a04d55220d0aa2e68b09c5aa0df796a54892
TingLinTL <Tarynlin0101@gmail.com>
2025-06-09 01:18:26 -0400

    revised G-formula

--- weibull_revised/simulation_wb_2.R
+++ weibull_revised/simulation_wb_2.R
@@ -14,48 +14,6 @@ cp_withU <- cp_withoutU <- cp_huang <- logical(n_sim)
 ipw_spce_withU <- ipw_spce_withoutU <- numeric(n_sim)
 
 
-#function for computing SPCE
-#with U
-compute_spce_withU <- function(data) {
-  # Fit seperated weibull AFT models for A=1 and A=0 by using sub-dataset
-  model1 <- survreg(Surv(M, status) ~ x1 + x2 + U, data = subset(data, A == 1), dist = "weibull")
-  model0 <- survreg(Surv(M, status) ~ x1 + x2 + U, data = subset(data, A == 0), dist = "weibull")
-  
-  # Predict survival probability at specific time point over entire dataset
-  mu1 <- predict(model1, newdata = data, type = "lp")
-  mu0 <- predict(model0, newdata = data, type = "lp")
-  sigma1 <-  model1$scale
-  sigma0 <-  model0$scale
-  # S1 <- exp(-exp((log(t_pred)-mu1)/sigma1))
-  # S0 <- exp(-exp((log(t_pred)-mu0)/sigma0))
-  S1 <- exp(-(exp(-mu1) * t_pred)^(1/sigma1))
-  S0 <- exp(-(exp(-mu0) * t_pred)^(1/sigma0))
-
-  #log-normal aft
-  # S1 <- 1 - pnorm((log(t_pred) - mu1) / model1$scale, mean=0, sd=1) #this only works for log-normal aft, not weibull
-  # S0 <- 1 - pnorm((log(t_pred) - mu0) / model0$scale, mean=0, sd=1) 
-  #S(t)= 1- phi((log(t)-mu)/sigma), phi is CDF of standard normal
-  #OR S <- 1 - plnorm(t_pred, meanlog = mu, sdlog = model$scale) 
-  
-  return(mean(S1) - mean(S0)) # Estimate SPCE
-}
-
-compute_spce_withoutU <- function(data) {
-  # Fit seperated weibull AFT models for A=1 and A=0 by using sub-dataset
-  model1 <- survreg(Surv(M, status) ~ x1 + x2 , data = subset(data, A == 1), dist = "weibull")
-  model0 <- survreg(Surv(M, status) ~ x1 + x2 , data = subset(data, A == 0), dist = "weibull")
-  # Predict survival probability at specific time point over entire dataset
-  mu1 <- predict(model1, newdata = data, type = "lp")
-  mu0 <- predict(model0, newdata = data, type = "lp")
-  sigma1 <-  model1$scale
-  sigma0 <-  model0$scale
-  # S1 <- exp(-exp((log(t_pred)-mu1)/sigma1)) 
-  # S0 <- exp(-exp((log(t_pred)-mu0)/sigma0))
-  S1 <- exp(-(exp(-mu1) * t_pred)^(1/sigma1))
-  S0 <- exp(-(exp(-mu0) * t_pred)^(1/sigma0))
-  return(mean(S1) - mean(S0))
-}
-
 #simulation
 for (i in 1:n_sim) {
   #Generate X1, X2, U
@@ -149,8 +107,9 @@ for (i in 1:n_sim) {
   )
   
   #Point estimate
-  spce_withU[i] <- compute_spce_withU(data_sim)
-  spce_withoutU[i] <- compute_spce_withoutU(data_sim) 
+  source("G_function.R")
+  spce_withU[i] <- Gcompute_spce_withU(data_sim)
+  spce_withoutU[i] <- Gcompute_spce_withoutU(data_sim) 
   
   #IPTW with/without U fpr SPCE
   #questions??? IPTW is nonparametric for marginal potential survival curves- KM estimator
